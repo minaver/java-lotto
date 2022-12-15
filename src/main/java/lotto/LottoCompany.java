c44b9373b3ce7483a9b48ddb4be73f1e568cdfbc
minaver <psmjoshua@gmail.com>
2022-12-15 14:04:24 +0900

    feat(LottoCompany): 전체 수익 금액 계산 메소드 추가

--- src/main/java/lotto/LottoCompany.java
+++ src/main/java/lotto/LottoCompany.java
@@ -1,4 +1,35 @@
 package lotto;
 
+import lotto.enums.Grade;
+import lotto.vo.WinLotto;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class LottoCompany {
+    private WinLotto winLotto;
+
+    public LottoCompany(WinLotto winLotto) {
+        this.winLotto = winLotto;
+    }
+
+    public Map<Grade,Integer> getWinStats(List<Lotto> payedLotto) {
+        return null;
+    }
+
+    public Double getEarningRate(int money, Map<Grade,Integer> winStats) {
+        return null;
+    }
+
+    private int calcTotalEarningMoney(Map<Grade,Integer> winStats) {
+        winStats.entrySet().stream()
+                .filter(v -> v.getValue() != 0)
+                .collect(Collectors.summingInt(v -> v.getKey().number()));
+        return 0;
+    }
+
+    private Double calcEarningRate(int money, int earningMoney) {
+        return 0.0;
+    }
 }
