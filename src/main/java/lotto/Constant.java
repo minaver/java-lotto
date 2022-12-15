c4e6de16e265fab146d92dff17868eb53f03d8af
minaver <psmjoshua@gmail.com>
2022-12-15 14:47:00 +0900

    feat(Constant): error 메시지 추가

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -9,4 +9,9 @@ public class Constant {
     public static final int thirdCorrectNum = 5;
     public static final int forthCorrectNum = 4;
     public static final int fifthCorrectNum = 3;
+
+    public static final int moneyUnit = 1000;
+
+    public static final String errorSmallSizeMoney = "[ERROR] 로또를 살수 없는 금액입니다.";
+    public static final String errorWrongSizeMoney = "[ERROR] 로또를 살수 없는 금액입니다.";
 }
