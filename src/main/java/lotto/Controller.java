3bc6799d4ae6b7089a2b501a668b90ae58134157
minaver <psmjoshua@gmail.com>
2022-12-15 16:08:53 +0900

    feat(Controller): getLottos 메서드 추가

---
+++ src/main/java/lotto/Controller.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import lotto.views.InputView;
+import lotto.views.OutputView;
+import lotto.vo.WinLotto;
+
+import java.util.List;
+
+public class Controller {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Shop shop;
+    private final LottoCompany lottoCompany;
+    private List<Lotto> payedLottos;
+    private WinLotto winLotto;
+
+    public Controller(InputView inputView, OutputView outputView, Shop shop, LottoCompany lottoCompany) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.shop = shop;
+        this.lottoCompany = lottoCompany;
+    }
+
+    public List<Lotto> getLottos() {
+        int payedMoney = inputView.getPayedMoney();
+        List<Lotto> payedLotto = shop.getPayedLotto(payedMoney);
+        outputView.printPayedLotto(payedLottos);
+        return payedLotto;
+    }
+    
+}
