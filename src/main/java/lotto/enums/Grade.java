c714bddb92219b10086db8bdd10aa69fa2f927ed
minaver <psmjoshua@gmail.com>
2022-12-15 15:51:59 +0900

    fix(Grade): correct enum 추가

--- src/main/java/lotto/enums/Grade.java
+++ src/main/java/lotto/enums/Grade.java
@@ -6,22 +6,28 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Grade {
-    first(2000000000),
-    second(30000000),
-    third(1500000),
-    forth(50000),
-    fifth(5000),
-    none(0)
+    first(6,2000000000),
+    second(5,30000000),
+    third(5,1500000),
+    forth(4,50000),
+    fifth(3,5000),
+    none(0,0)
     ;
 
+    private final int correct;
     private final int number;
 
-    Grade(int number) {
+    Grade(int correct, int number) {
+        this.correct = correct;
         this.number = number;
     }
 
     public int number() {
         return number;
+    }
+
+    public int correct() {
+        return correct;
     }
 
     private static final Map<Integer, Grade> BY_NUMBER=
