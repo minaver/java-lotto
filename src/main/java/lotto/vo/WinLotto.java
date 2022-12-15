d3e270e6c0e70533e637132c1281f82c7fcacd2d
minaver <psmjoshua@gmail.com>
2022-12-15 13:49:29 +0900

    feat(WinLotto): WinLotto VO 생성

--- src/main/java/lotto/vo/WinLotto.java
+++ src/main/java/lotto/vo/WinLotto.java
@@ -1,4 +1,21 @@
 package lotto.vo;
 
+import lotto.Lotto;
+
 public class WinLotto {
+    private Lotto winLotto;
+    private int bonusNumber;
+
+    public WinLotto(Lotto winLotto, int bonusNumber) {
+        this.winLotto = winLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinLotto() {
+        return winLotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
