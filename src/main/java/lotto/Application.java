97bdfe78ca15a6e30418ae92f17d76557f458d52
minaver <psmjoshua@gmail.com>
2022-12-15 16:21:00 +0900

    feat(Application): main 함수 logic 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.views.InputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller();
+
+        try {
+            List<Lotto> payedLotto = controller.getLottos();
+            controller.setWinLotto();
+            controller.getStats(payedLotto);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
