e39471cd6516ae1251d4932d09075f7e1b68398a
minaver <psmjoshua@gmail.com>
2022-12-15 13:53:49 +0900

    feat(LottoGenerator): LottoGenerator 객체 생성

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,4 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class LottoGenerator {
+
+    public Lotto generateLotto(List<Integer> targetNumber) {
+        return new Lotto(targetNumber);
+    }
+
+    public Lotto generateLottoRandom() {
+        List<Integer> targetNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(targetNumber);
+    }
+
 }
