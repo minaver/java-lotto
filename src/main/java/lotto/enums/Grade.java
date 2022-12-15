efad351633ed8931d10fe7b94b2af6548c82d313
minaver <psmjoshua@gmail.com>
2022-12-15 13:48:05 +0900

    feat(Grade): Grade 객체 생성

--- src/main/java/lotto/enums/Grade.java
+++ src/main/java/lotto/enums/Grade.java
@@ -1,4 +1,33 @@
 package lotto.enums;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Grade {
+    first(2000000000),
+    second(30000000),
+    third(1500000),
+    forth(50000),
+    fifth(5000)
+    ;
+
+    private final int number;
+
+    Grade(int number) {
+        this.number = number;
+    }
+
+    public int number() {
+        return number;
+    }
+
+    private static final Map<Integer, Grade> BY_NUMBER=
+            Stream.of(values()).collect(Collectors.toMap(Grade::number, Function.identity()));
+
+    public static Grade valueOfNumber(int number) {
+        return BY_NUMBER.get(number);
+    }
+
 }
