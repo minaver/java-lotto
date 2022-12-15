3517ca6d6ad07c6390fe7d611b4b1223607ad1bd
minaver <psmjoshua@gmail.com>
2022-12-15 14:47:37 +0900

    feat(Shop): moneySizeValidate 메서드 추가

--- src/main/java/lotto/Shop.java
+++ src/main/java/lotto/Shop.java
@@ -1,4 +1,45 @@
 package lotto;
 
+import lotto.vo.WinLotto;
+
+import java.util.List;
+
 public class Shop {
+    private LottoGenerator lottoGenerator;
+
+    public Shop(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
+    public List<Lotto> getPayedLotto(int money) {
+
+        return null;
+    }
+
+    public WinLotto getWinLotto(List<Integer> targetNumber, int bonusNumber) {
+        return null;
+    }
+
+    private int getLottoCount(int money) {
+        return money / Constant.moneyUnit;
+    }
+
+    private void moneySizeValidate(int money) {
+        if (money < Constant.moneyUnit) {
+            System.out.println(Constant.errorSmallSizeMoney);
+            throw new IllegalArgumentException();
+        }
+        if (money % Constant.moneyUnit == 0) {
+            System.out.println(Constant.errorWrongSizeMoney);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void moneyTypeValidate(int money) {
+
+    }
+
+    private void bonusNumValidate(int bonusNumber) {
+
+    }
 }
