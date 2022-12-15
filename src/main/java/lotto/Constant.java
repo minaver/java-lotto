99ce2aa6f819713a395f8a8aa713413ab13f84ce
minaver <psmjoshua@gmail.com>
2022-12-15 15:18:21 +0900

    feat(Constant): error 메시지 추가

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -14,6 +14,14 @@ public class Constant {
 
     public static final int moneyUnit = 1000;
 
+    public static final String inputPayingMoney = "구입금액을 입력해 주세요.";
+    public static final String inputWinLottoNum = "당첨 번호를 입력해 주세요.";
+    public static final String inputBonusNum = "보너스 번호를 입력해 주세요.";
+
+    public static final String outputPayedLotto = "8개를 구매했습니다.";
+    public static final String outputWinStat = "당첨 통계\n---\n";
+
+    public static final String errorInvalidTypeMoney = "[ERROR] 잘못된 타입입니다.";
     public static final String errorSmallSizeMoney = "[ERROR] 금액이 너무 적습니다.";
     public static final String errorWrongSizeMoney = "[ERROR] 로또를 살수 없는 금액입니다.";
     public static final String errorWrongBonusNum = "[ERROR] 잘못된 보너스 숫자입니다.";
