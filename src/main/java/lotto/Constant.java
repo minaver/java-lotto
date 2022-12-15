27605481c17a888b35d2f959ceb086eff897df31
minaver <psmjoshua@gmail.com>
2022-12-15 14:35:36 +0900

    feat(Constant): lotto 번호 관련 상수 추가

---
+++ src/main/java/lotto/Constant.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Constant {
+
+    public static final int lottoSize = 6;
+
+    public static final int firstCorrectNum = 6;
+    public static final int secondCorrectNum = 5;
+    public static final int thirdCorrectNum = 5;
+    public static final int forthCorrectNum = 4;
+    public static final int fifthCorrectNum = 3;
+}
