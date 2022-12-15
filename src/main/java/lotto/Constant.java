c490fa43c0f5a40feb3e8e4489d6028db65eecff
minaver <psmjoshua@gmail.com>
2022-12-15 15:00:56 +0900

    feat(Constant): error 메시지 추가

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -3,6 +3,8 @@ package lotto;
 public class Constant {
 
     public static final int lottoSize = 6;
+    public static final int lottoNumRangeStart = 1;
+    public static final int lottoNumRangeEnd = 45;
 
     public static final int firstCorrectNum = 6;
     public static final int secondCorrectNum = 5;
@@ -12,6 +14,7 @@ public class Constant {
 
     public static final int moneyUnit = 1000;
 
-    public static final String errorSmallSizeMoney = "[ERROR] 로또를 살수 없는 금액입니다.";
+    public static final String errorSmallSizeMoney = "[ERROR] 금액이 너무 적습니다.";
     public static final String errorWrongSizeMoney = "[ERROR] 로또를 살수 없는 금액입니다.";
+    public static final String errorWrongBonusNum = "[ERROR] 잘못된 보너스 숫자입니다.";
 }
