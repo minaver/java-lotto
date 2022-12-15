f92d13f85a8233674ce54c01a1f31374c06fdad3
minaver <psmjoshua@gmail.com>
2022-12-15 15:27:34 +0900

    feat(InputView): getWinLottoNumber 메서드 추가

--- src/main/java/lotto/views/InputView.java
+++ src/main/java/lotto/views/InputView.java
@@ -3,7 +3,9 @@ package lotto.views;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.Constant;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -15,7 +17,12 @@ public class InputView {
     }
 
     public List<Integer> getWinLottoNumber() {
-
+        System.out.println(Constant.inputWinLottoNum);
+        String input = Console.readLine();
+        String[] split = input.split(",");
+        return Arrays.asList(split).stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     public int getBonusNumber() {
