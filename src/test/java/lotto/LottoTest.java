bb81ff53deb3c87b14de54f88c06af135f1247c6
minaver <psmjoshua@gmail.com>
2022-12-15 17:44:38 +0900

    test(Lotto): 로또 번호가 지정된 범위 이외의 경우 예외 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 지정된 범위보다 작으면 예외가 발생한다.")
+    @Test
+    void 로또_번호_작은_범위_예외_테스트() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 지정된 범위보다 크면 예외가 발생한다.")
+    @Test
+    void 로또_번호_큰_범위_예외_테스트() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
