7ad9ecb43de165c326e5525dc89cb6a268414afc
minaver <psmjoshua@gmail.com>
2022-12-15 15:53:04 +0900

    feat(Output): printWinStats 메서드 추가

--- src/main/java/lotto/views/OutputView.java
+++ src/main/java/lotto/views/OutputView.java
@@ -4,8 +4,11 @@ import lotto.Constant;
 import lotto.Lotto;
 import lotto.enums.Grade;
 
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
@@ -17,10 +20,31 @@ public class OutputView {
     }
 
     public void printWinStats(Map<Grade,Integer> winStats) {
+        System.out.println(Constant.outputWinStatTitle);
+        for (Map.Entry<Grade, Integer> entry : winStats.entrySet()) {
+            String prize = convertPrizeString(String.valueOf(entry.getKey().number()));
 
+            if (entry.getKey().equals(Grade.second)) {
+                System.out.printf(Constant.outputWinStatBonus,
+                        entry.getKey().correct(),prize,entry.getValue());
+                return;
+            }
+            System.out.printf(Constant.outputWinStat,
+                    entry.getKey().correct(),prize,entry.getValue());
+
+        }
     }
 
     public void printEarningRate(Double earningRate) {
 
+    }
+
+    private String convertPrizeString(String prize) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(prize);
+        for (int i = prize.length()-3; i > 0; i = i-3) {
+            sb.insert(i,",");
+        }
+        return sb.toString();
     }
 }
